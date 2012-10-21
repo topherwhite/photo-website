1d07c56161827adb116f2c477b0b752ebf8dced9
topherwhite <topherwhite@gmail.com>
2012-10-21 00:31:35 -0700

    added menu

--- inc/html/head.inc.php
+++ inc/html/head.inc.php
@@ -4,16 +4,25 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <meta name="google-site-verification" content="<?php echo $GLOBALS['app_google_site_verification']; ?>" />
 <title><?php echo $GLOBALS['app_title']; ?></title>
+
 <link rel="stylesheet" type="text/css" href="../lib/css-reset.css"></link>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+
 <link rel="stylesheet" type="text/css" href="../lib/galleriffic/basic.css"></link>
 <link rel="stylesheet" type="text/css" href="../lib/galleriffic/galleriffic-5.css"></link>
 <link rel="stylesheet" type="text/css" href="../lib/galleriffic/black.css"></link>
 <link rel="stylesheet" type="text/css" href="../css/styles.css"></link>
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
 <script src="../lib/galleriffic/jquery.galleriffic.js"></script>
 <script src="../lib/galleriffic/jquery.history.js"></script>
 <script src="../lib/galleriffic/jquery.opacityrollover.js"></script>
+
+<link rel="stylesheet" type="text/css" media="screen" href="../lib/menu/css/style.css" />
+<script src="../lib/menu/jquery.easing.1.3.js"></script>
+<script src="../lib/menu/js.js"></script>
+
+<link rel="stylesheet" type="text/css" href="../css/styles.css"></link>
 <script type="text/javascript" src="../js/scripts.js"></script>
+
 <!--[if gte IE 9]>
   <style type="text/css"> .gradient { filter:none; } </style>
 <![endif]-->
